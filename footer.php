111a1f18c7f8665db86390026a915ca234888157
xishma <xishma@gmail.com>
2017-03-10 16:02:31 +0330

    adds header and footer content, adds some missing files

--- footer.php
+++ footer.php
@@ -1,1 +1,68 @@
 <!-- Footer -->
+<footer id="footer">
+    <div class="container">
+        <div class="row aboutTed">
+            <div class="col-md-6">
+                <h3>About TED</h3>
+                <p> TED is a nonprofit organization devoted to Ideas Worth Spreading. Started as a conference in
+                    California 1984, TED has grown to support those world-changing ideas with many initiatives.At a TED
+                    conference, the world's leading thinkers and doers are asked to give the talk of their lives in 18
+                    minutes or less. TED speakers have included Roger Ebert, Sheryl Sandberg, Bill Gates, Elizabeth
+                    Gilbert, Benoit Mandelbrot, Philippe Starck, Ngozi Okonjo-Iweala, Brian Greene, Isabel Allende and
+                    former UK Prime Minister Gordon Brown. </p>
+            </div>
+            <div class="col-md-6">
+                <h3>What's TEDx?</h3>
+                <p> In the spirit of ideas worth spreading, TED has created a program called TEDx. TEDx is a program of
+                    local, self-organized events that bring people together to share a TED-like experience. Our event is
+                    called TEDxTehran, where x = independently organized TED event. At our TEDxTehran event, TEDTalks
+                    video and live speakers will combine to spark deep discussion and connection in a small group. The
+                    TED Conference provides general guidance for the TEDx program, but individual TEDx events, including
+                    ours, are self-organized. </p>
+            </div>
+        </div>
+        <div class="row copyright">
+            <!-- Menu -->
+            <div class="col-md-9 col-sm-12 col-xs-12">
+                <p>
+                    This independent TEDx event is operated under license from TED. &copy; TEDxYouth @Tehran 2017. All
+                    Rights Reserved.
+                </p>
+            </div>
+            <div class="col-md-3 col-sm-12 col-xs-12">
+                <ul class="actions">
+                    <li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+                    <li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+                    <li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+                    <li><a href="#" class="icon fa-linkedin"><span class="label">LinkedIn</span></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- Scripts -->
+<script src="<?php bloginfo('template_directory'); ?>/js/jquery.min.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>/js/bootstrap.min.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>/js/jquery.poptrox.min.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>/js/jquery.scrollex.min.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>/js/skel.min.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>/js/util.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>/js/sketch.js"></script>
+<!--[if lte IE 8]>
+<script src="<?php bloginfo('template_directory'); ?>/js/ie/respond.min.js"></script><![endif]-->
+<script src="<?php bloginfo('template_directory'); ?>/js/menures.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>/js/main.js"></script>
+<script type="text/javascript">
+    $(function () {
+        $.each(['#f00', '#ff0', '#0f0', '#0ff', '#00f', '#f0f', '#000', '#fff'], function () {
+            $('#sketch-toolbar .tools').append("<a href='#colors_sketch' data-tool='marker' data-color='" + this + "' style='background: " + this + ";'></a> ");
+        });
+        $.each([3, 5, 10, 15], function () {
+            $('#sketch-toolbar .tools').append("<a href='#colors_sketch' data-tool='marker' data-size='" + this + "' style='background: #ccc;'>" + this + "</a> ");
+        });
+        $('#colors_sketch').sketch();
+    });
+</script>
+</body>
+
+</html>
--- header.php
+++ header.php
@@ -1,1 +1,48 @@
+<!DOCTYPE HTML>
+<!--
+  84 69 68 120 89 111 117 116 104 64 84 101 104 114 97 110
+-->
+<html>
+
+<head>
+    <title>TEDxYouth@Tehran</title>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <!--[if lte IE 8]>
+    <script src="<?php bloginfo('template_directory'); ?>/js/ie/html5shiv.js"></script><![endif]-->
+    <!-- Favicons -->
+    <link rel="apple-touch-icon" sizes="144x144" href="<?php bloginfo('template_directory'); ?>/images/favicons/apple-touch-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="<?php bloginfo('template_directory'); ?>/images/favicons/apple-touch-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="<?php bloginfo('template_directory'); ?>/images/favicons/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="57x57" href="<?php bloginfo('template_directory'); ?>/images/favicons/apple-touch-icon-57x57.png">
+    <link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/images/favicons/favicon.png" type="image/png">
+    <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/main.css"/>
+    <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/custom.css"/>
+    <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/hover-min.css"/>
+    <!--[if lte IE 8]>
+    <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/ie8.css"/><![endif]-->
+    <!--[if lte IE 9]>
+    <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/ie9.css"/><![endif]-->
+</head>
+
+<body>
 <!-- Header -->
+<header id="header">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <a href="#" class="logo" data-scroll><img alt="logo" height="50" id="logo" src="<?php bloginfo('template_directory'); ?>/images/logo.png"></a>
+                <nav class="nav-collapse">
+                    <ul>
+                        <li class="menu-item active"><a href="#intro" data-scroll>Home</a></li>
+                        <li class="menu-item"><a href="#about" data-scroll>About</a></li>
+                        <li class="menu-item"><a href="#getInvolved" data-scroll>Get Involved</a></li>
+                        <li class="menu-item"><a href="#talks" data-scroll>Talks</a></li>
+                        <li class="menu-item"><a href="#sketch-sec" data-scroll>Wall</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
