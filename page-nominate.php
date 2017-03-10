09f121a2d4f0a7871529a5ed142a0533b65aae58
xishma <xishma@gmail.com>
2017-03-10 17:07:54 +0330

    adds static nominate page

--- page-nominate.php
+++ page-nominate.php
@@ -5,6 +5,43 @@
 
 <?php get_header(); ?>
 
-<!---->
+<!-- pageConatiner -->
+<section id="pageConatiner">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="vid whyTedxVid">
+                    <iframe src="https://embed.ted.com/talks/chris_anderson_teds_secret_to_great_public_speaking"
+                            width="100%" height="400" frameborder="0" scrolling="no" webkitAllowFullScreen
+                            mozallowfullscreen allowFullScreen></iframe>
+                </div>
+            </div>
+            <div class="col-md-6 WhyTedxText">
+                <h2 class="titlePages">Nominate for <span class="TEDxRed">TEDxYouth 2017</span></h2>
+                <h5> Has this person contributed to Iran in the following areas: </h5>
+                <p>TEDxYouth@Tehran is part of the TEDxYouth initiative by TED. On and around the time of Universal
+                    Children’s Day, several cities and communities host a TEDxYouth event. While each event is different
+                    in format, size and theme, they have a shared vision: inspiring creativity,
+                    <br> igniting innovation, and empowering the youth. TEDxYouth@Tehran is based on a central premise
+                    that every young person is the captain of their destiny, the master of their life. They need to take
+                    responsibility for their life, be independent thinkers and explore their imagination. </p>
+            </div>
+        </div>
+        <div class="row nominateForm">
+            <div class="col-md-12 nominateFormTitle">
+                <h2 class="titlePagesCenter">Nominate the Next TEDxYouth</h2> Blandit varius ut praesent nascetur eu
+                penatibus nisi risus faucibus nunc.
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <iframe id="widget_iframe" src="https://www.eventbox.ir/forms/show/535" frameborder="0"
+                        class="widget_iframe" style="min-height:370px;width:100%;height:800px;" vspace="0" hspace="0"
+                        marginheight="5" marginwidth="5" scrolling="auto" allowtransparency="true">
+                </iframe>
+            </div>
+        </div>
+    </div>
+</section>
 
 <?php get_footer(); ?>
